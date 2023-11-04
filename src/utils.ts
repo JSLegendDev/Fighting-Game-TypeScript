c738834f7e3edc767f67157eb75f992f6a652d90
Nassim <jslegend@protonmail.com>
2023-11-04 14:10:12 -0400

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,34 @@
-function loadMap() {}
+import { KaboomCtx, GameObj } from "kaboom";
+
+export async function fetchMapData(mapFilePath: string) {
+  return await (await fetch(mapFilePath)).json();
+}
+
+export function drawTiles(
+  k: KaboomCtx,
+  map: GameObj,
+  layer,
+  tileheight,
+  tilewidth
+) {
+  let nbOfDrawnTiles = 0;
+  const tilePos = k.vec2(0, 0);
+  for (const tile of layer.data) {
+    if (nbOfDrawnTiles % layer.width === 0) {
+      tilePos.x = 0;
+      tilePos.y += tileheight;
+    } else {
+      tilePos.x += tilewidth;
+    }
+
+    nbOfDrawnTiles++;
+
+    if (tile === 0) continue;
+
+    map.add([
+      k.sprite("tileset", { frame: tile - 1 }),
+      k.pos(tilePos),
+      k.offscreen(),
+    ]);
+  }
+}
