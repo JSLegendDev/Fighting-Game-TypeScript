64dab8c39a386f67e5b2fa6895ae35bebb88aee7
Nassim <jslegend@protonmail.com>
2023-11-14 10:43:09 -0500

    revamped animation logic because it was a mess wip

--- src/entities/fighter.ts
+++ src/entities/fighter.ts
@@ -4,20 +4,16 @@ import { Directions, Entity } from "../types";
 export const fighterProps: {
   speed: number;
   direction: null | string;
-  isAttacking: boolean;
-  isRunning: boolean;
-  isDead: boolean;
-  isHit: boolean;
+  isUnskipAnimPlaying: boolean;
+  isSkipAnimPlaying: boolean;
   maxHp: number;
   previousHp: number;
   previousHeight: number;
 } = {
   speed: 200,
   direction: null,
-  isAttacking: false,
-  isRunning: false,
-  isDead: false,
-  isHit: false,
+  isUnskipAnimPlaying: false,
+  isSkipAnimPlaying: false,
   maxHp: 10,
   previousHp: 10,
   previousHeight: 0,
@@ -47,133 +43,116 @@ export function setFighterControls(
   fighter: GameObj,
   keys: { LEFT: string; RIGHT: string; UP: string; DOWN: string }
 ) {
-  const movementControlsRef = k.onKeyDown((key) => {
-    switch (key) {
-      case keys.LEFT:
-        fighter.isRunning = true;
-        fighter.flipX = true;
-        fighter.move(-fighter.speed, 0);
-        fighter.direction = Directions.LEFT;
-        break;
-      case keys.RIGHT:
-        fighter.isRunning = true;
-        fighter.flipX = false;
-        fighter.move(fighter.speed, 0);
-        fighter.direction = Directions.RIGHT;
-        break;
-      default:
+  k.onKeyDown((key) => {
+    if (fighter.isUnskipAnimPlaying) return;
+
+    if (key === keys.LEFT) {
+      fighter.flipX = true;
+      fighter.move(-fighter.speed, 0);
+      fighter.direction = Directions.LEFT;
+      if (fighter.curAnim() !== "run") {
+        fighter.play("run");
+        fighter.isSkipAnimPlaying = true;
+      }
+      return;
+    }
+
+    if (key === keys.RIGHT) {
+      fighter.flipX = false;
+      fighter.move(fighter.speed, 0);
+      fighter.direction = Directions.RIGHT;
+      if (fighter.curAnim() !== "run") {
+        fighter.play("run");
+        fighter.isSkipAnimPlaying = true;
+      }
+      return;
     }
   });
 
-  const actionsControlsRef = k.onKeyPress((key) => {
-    switch (key) {
-      case keys.UP:
-        if (fighter.isGrounded()) fighter.jump();
-        break;
-      case keys.DOWN:
-        fighter.isAttacking = true;
-
-        function updateHitboxPos() {
-          const fighterPos = fighter.worldPos();
-          const hitboxPos: { [key: string]: Vec2 } = {
-            LEFT: k.vec2(fighterPos.x - 50, fighterPos.y),
-            RIGHT: k.vec2(fighterPos.x + 50, fighterPos.y),
-          };
-          return hitboxPos[fighter.direction];
-        }
-
-        const attackHitbox = k.add([
-          k.area({ shape: new k.Rect(k.vec2(0), 50, 50) }),
-          k.anchor("center"),
-          k.pos(updateHitboxPos()),
-        ]);
-
-        const enemyTag = fighter.is("samurai") ? "ninja" : "samurai";
-
-        attackHitbox.onCollide(enemyTag, (enemy) => {
-          enemy.previousHp = enemy.hp();
-          k.wait(0.1, () => (enemy.previousHp = enemy.hp()));
-          if (enemy.hp() !== 0) {
-            enemy.hurt(1);
-            enemy.isHit = true;
-            return;
-          }
-
-          enemy.isDead = true;
-        });
-
-        const attackUpdateRef = k.onUpdate(() => {
-          attackHitbox.pos = updateHitboxPos();
-
-          if (!fighter.isAttacking) {
-            k.destroy(attackHitbox);
-            attackUpdateRef.cancel();
-          }
-        });
-
-        break;
-      default:
+  k.onKeyRelease((key) => {
+    if (key === keys.LEFT || key === keys.RIGHT) {
+      fighter.isSkipAnimPlaying = false;
     }
   });
 
-  k.onKeyRelease(() => {
-    fighter.isRunning = false;
-  });
-
-  const animationManagerRef = k.onUpdate(() => {
-    if (fighter.isAttacking && fighter.curAnim() !== "attack") {
-      fighter.play("attack", {
-        onEnd() {
-          fighter.isAttacking = false;
-        },
-      });
-    }
-
-    if (fighter.isJumping() && fighter.curAnim() !== "jump")
-      fighter.play("jump");
-
-    if (fighter.isFalling() && fighter.curAnim() !== "fall")
-      fighter.play("fall");
-
+  k.onKeyPress((key) => {
     if (
-      fighter.isRunning &&
-      !fighter.isJumping() &&
-      !fighter.isFalling() &&
-      !fighter.isAttacking &&
-      fighter.curAnim() !== "run"
-    )
-      fighter.play("run");
-
-    if (fighter.isHit && fighter.curAnim() !== "hit") {
-      fighter.play("hit", {
-        onEnd() {
-          fighter.isHit = false;
-        },
-      });
-    }
-
-    if (fighter.isDead && fighter.curAnim() !== "death") {
-      movementControlsRef.cancel();
-      actionsControlsRef.cancel();
-
-      // Needed since isGrounded need area comp to still exist or
-      // will crash game
-      fighter.isStatic = true;
-      fighter.area.shape.height = 0;
-      fighter.play("death", {
-        onEnd() {
-          animationManagerRef.cancel();
-        },
-      });
-    }
-
-    if (
-      !fighter.isAttacking &&
-      !fighter.isRunning &&
+      key === keys.UP &&
       fighter.isGrounded() &&
-      !fighter.isHit &&
-      !fighter.isDead &&
-      fighter.curAnim() !== "idle"
+      fighter.curAnim() !== "jump"
+    ) {
+      fighter.jump();
+      fighter.play("jump");
+      fighter.isUnskipAnimPlaying = true;
+    }
+
+    if (key === keys.DOWN) {
+      function updateHitboxPos() {
+        const fighterPos = fighter.worldPos();
+        const hitboxPos: { [key: string]: Vec2 } = {
+          LEFT: k.vec2(fighterPos.x - 50, fighterPos.y),
+          RIGHT: k.vec2(fighterPos.x + 50, fighterPos.y),
+        };
+        return hitboxPos[fighter.direction];
+      }
+
+      const attackHitbox = k.add([
+        k.area({ shape: new k.Rect(k.vec2(0), 50, 50) }),
+        k.anchor("center"),
+        k.pos(updateHitboxPos()),
+      ]);
+
+      const enemyTag = fighter.is("samurai") ? "ninja" : "samurai";
+
+      attackHitbox.onCollide(enemyTag, (enemy) => {
+        k.wait(0.1, () => (enemy.previousHp = enemy.hp()));
+        if (enemy.hp() !== 0) {
+          enemy.hurt(1);
+          return;
+        }
+      });
+
+      const attackUpdateRef = k.onUpdate(() => {
+        attackHitbox.pos = updateHitboxPos();
+      });
+
+      if (fighter.curAnim() !== "attack") {
+        fighter.isUnskipAnimPlaying = true;
+        fighter.play("attack", {
+          onEnd: () => {
+            fighter.isUnskipAnimPlaying = false;
+          },
+        });
+      }
+      k.wait(0.3, () => {
+        k.destroy(attackHitbox);
+        attackUpdateRef.cancel();
+      });
+    }
+  });
+
+  k.onUpdate(() => {
+    // console.log({
+    //   skipAnimPlaying: fighter.isSkipAnimPlaying,
+    //   unSkipAnimPlaying: fighter.isUnskipAnimPlaying,
+    // });
+
+    if (
+      !fighter.isJumping() &&
+      !fighter.isGrounded() &&
+      fighter.curAnim() !== "fall"
+    ) {
+      fighter.play("fall");
+    }
+
+    if (fighter.curAnim() === "fall" && fighter.isGrounded()) {
+      fighter.isUnskipAnimPlaying = false;
+    }
+
+    if (
+      fighter.curAnim() !== "idle" &&
+      !fighter.isUnskipAnimPlaying &&
+      !fighter.isSkipAnimPlaying
     ) {
       fighter.play("idle");
     }
